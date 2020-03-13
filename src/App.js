7d6b14e8432c267e53c35b1d509d033645527d4a
Nadine Ju <nadine.jura@online.de>
2020-03-13 12:47:20 -0700

    helped Eddie and Piper to carry all their unimportant stuff himself

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
+import Balance from './components/Balance';
+import IncExp from './components/IncExp';
 
 function App() {
   return (
     <div>
       <Header />
+      <div className="container">
+        <Balance />
+        <IncExp />
+      </div>
     </div>
   );
 }
