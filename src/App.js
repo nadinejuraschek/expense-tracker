0aba0d1fe14e48bf15b63db667b58ddffbdfaa6b
Nadine Ju <nadine.jura@online.de>
2020-03-13 12:36:27 -0700

    Header component and css added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Header from './components/Header';
+
 function App() {
   return (
     <div>
-      App
+      <Header />
     </div>
   );
 }
